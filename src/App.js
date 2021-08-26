f0945c63abc4210255df9c435558fa15f2dcbe65
bharath <kadiri2006@gmail.com>
2021-08-26 12:39:11 +0530

    displaying data using jsx & with babel syntax & write js code in return

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import React from "react";
 import "./App.css";
 
 function App() {
+  let p = <p>this is paragraph tag link to p variable </p>;
+  let color = (
+    <p style={{ color: "black", fontSize: "30px" }}>
+      this paragraph tag is linked to color variable and used in line css
+      properties
+    </p>
+  ); //use inline styles in react like object
+  let babelsyntax = React.createElement(
+    "div",
+    null,
+    React.createElement("p", null, "this is babel conversion"),
+    React.createElement(
+      "p",
+      {
+        style:{color:"red",fontSize:"100px"},
+      },
+      "with two children"
+    )
+  );
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>hi iam in h1 element</h1>
+        {p}
+        {color}
+       {babelsyntax}
       </header>
     </div>
   );
