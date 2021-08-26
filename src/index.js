4159246fc1a4a8d620e4438102ca91a0bd3ca0a5
bharath <kadiri2006@gmail.com>
2021-08-26 11:56:38 +0530

    changed bg color,see different import syntax

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(<div>
-    <h1>iam bharath</h1>
-    <p>hi this is selected</p>
-</div>, document.getElementById("root"));
+ReactDOM.render(<App/>, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
