2b5a852b12ac758789395cdd70776be1732920ab
bharath <kadiri2006@gmail.com>
2021-08-28 12:03:26 +0530

    build weather app

--- src/App.js
+++ src/App.js
@@ -1,39 +1,49 @@
-import React from "react";
 import "./App.css";
+import logo from "./img/1.svg";
 
-function App() {
-  let p = <p>this is paragraph tag link to p variable </p>;
-  let instyle = <p>observe inline styles in react</p>;
-  let color = (
-    <p style={{ color: "black", fontSize: "30px" }}>
-      this paragraph tag is linked to color variable and used in line css
-      properties
-    </p>
-  ); //use inline styles in react like object
-  let babelsyntax = React.createElement(
-    "div",
-    null,
-    React.createElement("p", null, "this is babel conversion"),
-    React.createElement(
-      "p",
-      {
-        style: { color: "red", fontSize: "100px" },
-      },
-      "with two children"
-    )
-  );
+export default function weather() {
+  let data = [
+    {
+      day: "Monday",
+      date: "April 5th 1:00pm",
+      imga: logo,
+      temp: "35",
+      visibility: "clear sky",
+    },
+    {
+      day: "Tuesday",
+      date: "April 6th 10:00pm",
+      imga: logo,
+      temp: "20",
+      visibility: "cloudy",
+    },
+    {
+      day: "Wensday",
+      date: "April 7th 11:00pm",
+      imga: logo,
+      temp: "23",
+      visibility: "rainy",
+    },
+  ];
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>hi iam in h1 element</h1>
-        {p}
-        {color}
-        {babelsyntax}
-        {instyle}
-      </header>
+    <div className="body">
+      <h1> 3-day forecast</h1>
+      <p>Tirupathi Andhra pradesh,india </p>
+
+      {data.length > 1 ? (
+        data.map((value,index) => (
+          <div className="block" key={value.day}>
+            <p>{value.day}</p>
+            <p>{value.date}</p>
+            <p>{value.temp}</p>
+            <p>{value.visibility}</p>
+            <img src={value.imga} alt="cloud logo" />
+          </div>
+        ))
+      ) : (
+        <p>condition does't meet the requie ments</p>
+      )}
     </div>
   );
 }
-
-export default App;
